6ef041b401f4e86e13834a532d5edeba20b555d8
Ed Colen <ed.colen@gmail.com>
2021-01-18 14:23:55 -0300

    fixed pages controller

--- devise-bootstrap5.rb
+++ devise-bootstrap5.rb
@@ -62,7 +62,7 @@ after_bundle do
   # Generators: db + pages controller
   ########################################
   rails_command 'db:drop db:create db:migrate'
-  generate(:controller, 'pages home --skip-routes')
+  generate(:controller, 'pages', 'home')
 
   # Devise/Pundit install + user + views
   ########################################
