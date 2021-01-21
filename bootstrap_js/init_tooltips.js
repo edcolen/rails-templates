99021311db703501e7dfbccdc69b2c5963350ab1
Ed Colen <ed.colen@gmail.com>
2021-01-21 10:14:22 -0300

    added init tooltips

---
+++ bootstrap_js/init_tooltips.js
@@ -0,0 +1,18 @@
+const initTooltips = () => {
+    document.addEventListener("DOMContentLoaded", function(event) {
+        var popoverTriggerList = [].slice.call(
+            document.querySelectorAll('[data-bs-toggle="popover"]')
+        );
+        var popoverList = popoverTriggerList.map(function(popoverTriggerEl) {
+            return new bootstrap.Popover(popoverTriggerEl);
+        });
+        var tooltipTriggerList = [].slice.call(
+            document.querySelectorAll('[data-bs-toggle="tooltip"]')
+        );
+        var tooltipList = tooltipTriggerList.map(function(tooltipTriggerEl) {
+            return new bootstrap.Tooltip(tooltipTriggerEl);
+        });
+    });
+};
+
+export { initTooltips };
